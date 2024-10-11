0a3961ab16d61a07b37ad0ebe839127ab87a3a32
Islam412 <islam.124.hamdy@gmail.com>
2024-10-11 16:29:49 +0300

    create artical functions p1

---
+++ src/Artical.js
@@ -0,0 +1,11 @@
+export default function Artical() {
+    return (
+        <>
+            <div>
+                <h1>Islam</h1>
+                <h3>islam@gmail.com</h3>
+                <h3>2000</h3>
+            </div>
+        </>
+    )
+}
